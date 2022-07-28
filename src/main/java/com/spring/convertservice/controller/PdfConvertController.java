cb5daac52e5a5864ec2212c779cc2e8628e66844
albert-atabekyan <albert.atabekyan1200@gmail.com>
2022-07-28 18:59:28 +0300

    ConvertPDFController: fix bags(delete file param)

--- src/main/java/com/spring/convertservice/controller/PdfConvertController.java
+++ src/main/java/com/spring/convertservice/controller/PdfConvertController.java
@@ -4,15 +4,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping(path = "api/v1/fileToPDF")
 public class PdfConvertController {
 
     @PostMapping("/add")
-    public ResponseEntity<?> convertPDFFileEndpoint(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<?> convertPDFFileEndpoint() {
 
         return ResponseEntity.ok().build();
     }
