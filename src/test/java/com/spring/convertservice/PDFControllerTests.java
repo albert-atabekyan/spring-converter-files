b27f038f61940d3f9df57b7fd6a574f8959da6c3
albert-atabekyan <albert.atabekyan1200@gmail.com>
2022-07-28 20:55:01 +0300

    Create fist test in PDFControllerTests

--- src/test/java/com/spring/convertservice/PDFControllerTests.java
+++ src/test/java/com/spring/convertservice/PDFControllerTests.java
@@ -1,2 +1,29 @@
-package com.spring.convertservice;public class PDFControllerTests {
+package com.spring.convertservice;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.spring.convertservice.controller.PdfConvertController;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@SpringBootTest
+public class PDFControllerTests {
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setup() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(new PdfConvertController()).build();
+    }
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(post("http://localhost:8080/api/v1/fileToPDF/")).andDo(print()).andExpect(status().isOk());
+    }
 }
