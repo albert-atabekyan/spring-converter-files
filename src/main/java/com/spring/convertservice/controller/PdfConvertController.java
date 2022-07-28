3d91b2b9b6293068d84170206cfbb6635c6c5d98
albert-atabekyan <albert.atabekyan1200@gmail.com>
2022-07-28 18:51:08 +0300

    add endpoint for converting pdf

--- src/main/java/com/spring/convertservice/controller/PdfConvertController.java
+++ src/main/java/com/spring/convertservice/controller/PdfConvertController.java
@@ -1,4 +1,19 @@
 package com.spring.convertservice.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+@RequestMapping(path = "api/v1/fileToPDF")
 public class PdfConvertController {
+
+    @PostMapping("/add")
+    public ResponseEntity<?> convertPDFFileEndpoint(@RequestParam("file") MultipartFile file) {
+
+        return ResponseEntity.ok().build();
+    }
 }
