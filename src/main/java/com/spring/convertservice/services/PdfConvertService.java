6dd268434a37548557cec35f2e5d96413056c042
albert-atabekyan <albert.atabekyan1200@gmail.com>
2022-07-29 18:40:31 +0300

    PdfConvertService: create function for generating pdf from image

--- src/main/java/com/spring/convertservice/services/PdfConvertService.java
+++ src/main/java/com/spring/convertservice/services/PdfConvertService.java
@@ -1,5 +1,33 @@
 package com.spring.convertservice.services;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class PdfConvertService {
 
+    private OutputStream generatePDFFromImage(InputStream inputStream, String name) throws IOException {
+        PDPageContentStream contents;
+        PDDocument doc = new PDDocument();
+
+        PDImageXObject pdImage = PDImageXObject.createFromByteArray(doc, IOUtils.toByteArray(inputStream), name);
+
+        PDPage page = new PDPage();
+        doc.addPage(page);
+
+        contents = new PDPageContentStream(doc, page);
+        contents.drawImage(pdImage, 0.0F, 0.0F, pdImage.getWidth(), pdImage.getHeight());
+
+        OutputStream outputStream = doc.getDocument().createCOSStream().createOutputStream();
+
+        doc.close();
+
+        return outputStream;
+    }
 }
