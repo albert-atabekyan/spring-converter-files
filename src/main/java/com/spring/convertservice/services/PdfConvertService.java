f7547a2b30e6e8a545673899511a291e9d6b6f8f
albert-atabekyan <albert.atabekyan1200@gmail.com>
2022-07-29 20:28:02 +0300

    Ðfix bugs in function generatePDFFromImage

--- src/main/java/com/spring/convertservice/services/PdfConvertService.java
+++ src/main/java/com/spring/convertservice/services/PdfConvertService.java
@@ -7,26 +7,27 @@ import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 
 import java.io.*;
-import java.nio.file.Files;
 
 public class PdfConvertService {
 
-    public OutputStream generatePDFFromImage(File file) throws IOException {
+    public OutputStream generatePDFFromImage(byte[] array, String name) throws IOException {
         PDPageContentStream contents;
         PDDocument doc = new PDDocument();
 
-        PDImageXObject pdImage = PDImageXObject.createFromByteArray(doc, Files.readAllBytes(file.toPath()), file.getName());
+        PDImageXObject pdImage = PDImageXObject.createFromByteArray(doc, array, name);
 
         PDPage page = new PDPage();
         doc.addPage(page);
 
         contents = new PDPageContentStream(doc, page);
         contents.drawImage(pdImage, 0.0F, 0.0F, pdImage.getWidth(), pdImage.getHeight());
+        contents.close();
 
         OutputStream outputStream = new ByteArrayOutputStream();
 
         doc.save(outputStream);
 
+        outputStream.close();
         doc.close();
 
         return outputStream;
