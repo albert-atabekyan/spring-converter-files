d1f4c3d596715bdf3aec3f008018c5cf0cd76324
albert-atabekyan <albert.atabekyan1200@gmail.com>
2022-07-29 14:22:39 +0300

    Add parameter as file in request

--- src/main/java/com/spring/convertservice/controller/PdfConvertController.java
+++ src/main/java/com/spring/convertservice/controller/PdfConvertController.java
@@ -4,13 +4,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping(path = "api/v1/fileToPDF")
 public class PdfConvertController {
 
     @PostMapping("/")
-    public ResponseEntity<?> convertPDFFileEndpoint() {
+    public ResponseEntity<?> convertPDFFileEndpoint(@RequestParam MultipartFile file) {
 
         return ResponseEntity.ok().build();
     }
