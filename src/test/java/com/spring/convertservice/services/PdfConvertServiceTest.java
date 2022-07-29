8bce68f21fb6440b976b3804251785d84b54dfcb
albert-atabekyan <albert.atabekyan1200@gmail.com>
2022-07-29 20:48:53 +0300

    create test for generating pdf from image.

--- src/test/java/com/spring/convertservice/services/PdfConvertServiceTest.java
+++ src/test/java/com/spring/convertservice/services/PdfConvertServiceTest.java
@@ -1,4 +1,33 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.spring.convertservice.services;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@SpringBootTest
 class PdfConvertServiceTest {
-  
+
+    PdfConvertService convertService = new PdfConvertService();
+
+    @Test
+    public void testGeneratingPDFFromImage() throws Exception {
+        String name = "1";
+
+        Path imagePath = Paths.get("src/test/static/image/" + name + ".jpg");
+
+        InputStream image = Files.newInputStream(imagePath);
+
+        File resultFile = File.createTempFile("Test",".pdf");
+        ByteArrayOutputStream byteArrayOutputStream = (ByteArrayOutputStream) convertService.generatePDFFromImage(IOUtils.toByteArray(image), name);
+        try(OutputStream outputStream = new FileOutputStream(resultFile)) {
+            byteArrayOutputStream.writeTo(outputStream);
+        }
+
+        System.out.println("Please find your PDF File here: " + resultFile.getAbsolutePath());
+    }
 }
