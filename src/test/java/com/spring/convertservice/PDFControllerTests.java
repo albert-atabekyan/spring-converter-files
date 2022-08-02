694a0befba97ef2ad855a61f33d2d75b58e2432f
albert-atabekyan <albert.atabekyan1200@gmail.com>
2022-08-02 10:32:20 +0300

    add test for pdf controller: add png jpeg gif jpg pptx test

--- src/test/java/com/spring/convertservice/PDFControllerTests.java
+++ src/test/java/com/spring/convertservice/PDFControllerTests.java
@@ -14,9 +14,9 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import static org.assertj.core.api.Assertions.*;
 
-import java.io.File;
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
-import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @SpringBootTest
 public class PDFControllerTests {
@@ -30,16 +30,16 @@ public class PDFControllerTests {
 
     @Test
     public void DocxToPDFControllerTest() throws Exception {
-
         String endpoint = "/api/v1/fileToPDF";
 
-        InputStream fis = new FileInputStream(File.createTempFile("Test", "docx"));
+        String extension = "docx";
+        String dot = ".";
 
         MockMultipartFile multipartFile = new MockMultipartFile(
                 "file",
-                "file.docx",
+                "file" + dot + extension,
                 String.valueOf(MediaType.MULTIPART_FORM_DATA),
-                fis);
+                new ByteArrayInputStream("abc".getBytes(StandardCharsets.UTF_8)));
 
         HttpHeaders headers = new HttpHeaders();
 
@@ -54,5 +54,161 @@ public class PDFControllerTests {
                         .andReturn();
 
         assertThat(200).isEqualTo(result.getResponse().getStatus());
+        assertThat(MediaType.APPLICATION_PDF.toString()).isEqualTo(result.getResponse().getContentType());
+    }
+
+    @Test
+    public void PptxToPDFControllerTest() throws Exception {
+        String endpoint = "/api/v1/fileToPDF";
+
+        String extension = "pptx";
+        String dot = ".";
+
+        FileInputStream fis = new FileInputStream("src\\test\\static\\pptx\\1" + dot + extension);
+
+        MockMultipartFile multipartFile = new MockMultipartFile(
+                "file",
+                "file" + dot + extension,
+                String.valueOf(MediaType.MULTIPART_FORM_DATA),
+                fis);
+
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.add(HttpHeaders.ACCEPT, "*/*");
+        headers.add(HttpHeaders.ACCEPT_ENCODING, "gzip, deflate, br");
+        headers.add(HttpHeaders.CONNECTION, "keep-alive");
+
+        MvcResult result = mockMvc.perform(
+                        MockMvcRequestBuilders.multipart(endpoint)
+                                .file(multipartFile).headers(headers)
+                                .contentType(MediaType.MULTIPART_FORM_DATA))
+                .andReturn();
+
+        assertThat(200).isEqualTo(result.getResponse().getStatus());
+        assertThat(MediaType.APPLICATION_PDF.toString()).isEqualTo(result.getResponse().getContentType());
+    }
+
+    @Test
+    public void JpgToPDFControllerTest() throws Exception {
+        String endpoint = "/api/v1/fileToPDF";
+
+        String extension = "jpg";
+        String dot = ".";
+
+        FileInputStream fis = new FileInputStream("src\\test\\static\\image\\1" + dot + extension);
+
+        MockMultipartFile multipartFile = new MockMultipartFile(
+                "file",
+                "file" + dot + extension,
+                String.valueOf(MediaType.MULTIPART_FORM_DATA),
+                fis);
+
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.add(HttpHeaders.ACCEPT, "*/*");
+        headers.add(HttpHeaders.ACCEPT_ENCODING, "gzip, deflate, br");
+        headers.add(HttpHeaders.CONNECTION, "keep-alive");
+
+        MvcResult result = mockMvc.perform(
+                        MockMvcRequestBuilders.multipart(endpoint)
+                                .file(multipartFile).headers(headers)
+                                .contentType(MediaType.MULTIPART_FORM_DATA))
+                .andReturn();
+
+        assertThat(200).isEqualTo(result.getResponse().getStatus());
+        assertThat(MediaType.APPLICATION_PDF.toString()).isEqualTo(result.getResponse().getContentType());
+    }
+
+    @Test
+    public void JpegToPDFControllerTest() throws Exception {
+        String endpoint = "/api/v1/fileToPDF";
+
+        String extension = "jpeg";
+        String dot = ".";
+
+        FileInputStream fis = new FileInputStream("src\\test\\static\\image\\1" + dot + extension);
+
+        MockMultipartFile multipartFile = new MockMultipartFile(
+                "file",
+                "file" + dot + extension,
+                String.valueOf(MediaType.MULTIPART_FORM_DATA),
+                fis);
+
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.add(HttpHeaders.ACCEPT, "*/*");
+        headers.add(HttpHeaders.ACCEPT_ENCODING, "gzip, deflate, br");
+        headers.add(HttpHeaders.CONNECTION, "keep-alive");
+
+        MvcResult result = mockMvc.perform(
+                        MockMvcRequestBuilders.multipart(endpoint)
+                                .file(multipartFile).headers(headers)
+                                .contentType(MediaType.MULTIPART_FORM_DATA))
+                .andReturn();
+
+        assertThat(200).isEqualTo(result.getResponse().getStatus());
+        assertThat(MediaType.APPLICATION_PDF.toString()).isEqualTo(result.getResponse().getContentType());
+    }
+
+    @Test
+    public void GifToPDFControllerTest() throws Exception {
+        String endpoint = "/api/v1/fileToPDF";
+
+        String extension = "gif";
+        String dot = ".";
+
+        FileInputStream fis = new FileInputStream("src\\test\\static\\image\\1" + dot + extension);
+
+        MockMultipartFile multipartFile = new MockMultipartFile(
+                "file",
+                "file" + dot + extension,
+                String.valueOf(MediaType.MULTIPART_FORM_DATA),
+                fis);
+
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.add(HttpHeaders.ACCEPT, "*/*");
+        headers.add(HttpHeaders.ACCEPT_ENCODING, "gzip, deflate, br");
+        headers.add(HttpHeaders.CONNECTION, "keep-alive");
+
+        MvcResult result = mockMvc.perform(
+                        MockMvcRequestBuilders.multipart(endpoint)
+                                .file(multipartFile).headers(headers)
+                                .contentType(MediaType.MULTIPART_FORM_DATA))
+                .andReturn();
+
+        assertThat(200).isEqualTo(result.getResponse().getStatus());
+        assertThat(MediaType.APPLICATION_PDF.toString()).isEqualTo(result.getResponse().getContentType());
+    }
+
+    @Test
+    public void PngToPDFControllerTest() throws Exception {
+        String endpoint = "/api/v1/fileToPDF";
+
+        String extension = "png";
+        String dot = ".";
+
+        FileInputStream fis = new FileInputStream("src\\test\\static\\image\\1" + dot + extension);
+
+        MockMultipartFile multipartFile = new MockMultipartFile(
+                "file",
+                "file" + dot + extension,
+                String.valueOf(MediaType.MULTIPART_FORM_DATA),
+                fis);
+
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.add(HttpHeaders.ACCEPT, "*/*");
+        headers.add(HttpHeaders.ACCEPT_ENCODING, "gzip, deflate, br");
+        headers.add(HttpHeaders.CONNECTION, "keep-alive");
+
+        MvcResult result = mockMvc.perform(
+                        MockMvcRequestBuilders.multipart(endpoint)
+                                .file(multipartFile).headers(headers)
+                                .contentType(MediaType.MULTIPART_FORM_DATA))
+                .andReturn();
+
+        assertThat(200).isEqualTo(result.getResponse().getStatus());
+        assertThat(MediaType.APPLICATION_PDF.toString()).isEqualTo(result.getResponse().getContentType());
     }
 }
