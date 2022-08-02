1abf073de584e49995e22ddd9977376d509c6603
albert-atabekyan <albert.atabekyan1200@gmail.com>
2022-08-02 09:24:20 +0300

    changed test in PDFControllerTests in method DocxToPDFControllerTest

--- src/test/java/com/spring/convertservice/PDFControllerTests.java
+++ src/test/java/com/spring/convertservice/PDFControllerTests.java
@@ -1,30 +1,27 @@
 package com.spring.convertservice;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import com.spring.convertservice.controller.PdfConvertController;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.assertj.core.api.Assertions.*;
 
+import java.io.File;
 import java.io.FileInputStream;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.InputStream;
 
 @SpringBootTest
 public class PDFControllerTests {
 
     MockMvc mockMvc;
-
-    String BASE_URL = "http://localhost";
 
     @BeforeEach
     void setup() {
@@ -32,19 +29,30 @@ public class PDFControllerTests {
     }
 
     @Test
-    public void shouldReturnDefaultMessage() throws Exception {
+    public void DocxToPDFControllerTest() throws Exception {
 
-        String endpoint = BASE_URL + "/api/v1/fileToPDF/";
+        String endpoint = "/api/v1/fileToPDF";
 
-        FileInputStream fis = new FileInputStream("src/test/static/image/1.jpg");
-        System.out.println(fis);
+        InputStream fis = new FileInputStream(File.createTempFile("Test", "docx"));
 
-        MockMultipartFile multipartFile = new MockMultipartFile("file", fis);
+        MockMultipartFile multipartFile = new MockMultipartFile(
+                "file",
+                "file.docx",
+                String.valueOf(MediaType.MULTIPART_FORM_DATA),
+                fis);
 
-        MediaType mediaType = new MediaType("multipart", "form-data");
+        HttpHeaders headers = new HttpHeaders();
 
-        mockMvc.perform(
-                        MockMvcRequestBuilders.multipart(endpoint).file(multipartFile))
-                .andExpect(status().isOk());
+        headers.add(HttpHeaders.ACCEPT, "*/*");
+        headers.add(HttpHeaders.ACCEPT_ENCODING, "gzip, deflate, br");
+        headers.add(HttpHeaders.CONNECTION, "keep-alive");
+
+        MvcResult result = mockMvc.perform(
+                        MockMvcRequestBuilders.multipart(endpoint)
+                                .file(multipartFile).headers(headers)
+                                .contentType(MediaType.MULTIPART_FORM_DATA))
+                        .andReturn();
+
+        assertThat(200).isEqualTo(result.getResponse().getStatus());
     }
 }
