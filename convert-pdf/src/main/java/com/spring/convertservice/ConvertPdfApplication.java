e9d7e9182f8903bf4dc65253d6cb132425018654
albert-atabekyan <albert.atabekyan1200@gmail.com>
2022-08-07 16:08:15 +0300

    restructure project for creating microservice architecture

--- convert-pdf/src/main/java/com/spring/convertservice/ConvertPdfApplication.java
+++ convert-pdf/src/main/java/com/spring/convertservice/ConvertPdfApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ConvertPdfApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ConvertPdfApplication.class, args);
 	}
-
 }
