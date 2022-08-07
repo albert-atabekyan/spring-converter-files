10de2538b84e0dbf9d48e298bdf36494e2cc1762
albert-atabekyan <albert.atabekyan1200@gmail.com>
2022-08-07 16:46:21 +0300

    create convert router microservice

---
+++ convert-router/src/main/java/com/spring/convertrouter/ConvertRouterApplication.java
@@ -0,0 +1,13 @@
+package com.spring.convertrouter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertRouterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConvertRouterApplication.class, args);
+    }
+
+}
---
+++ convert-router/src/test/java/com/spring/convertrouter/ConvertRouterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.convertrouter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertRouterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
