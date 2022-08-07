5caf34176d07efd4aae67f2fe79abdf9d083a8ae
albert-atabekyan <albert.atabekyan1200@gmail.com>
2022-08-07 18:22:10 +0300

    create routing for converting to pdf

--- convert-router/src/main/java/com/spring/convertrouter/controller/RouterController.java
+++ convert-router/src/main/java/com/spring/convertrouter/controller/RouterController.java
@@ -1,18 +1,50 @@
 package com.spring.convertrouter.controller;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Objects;
 
 @RestController
 @RequestMapping(path = "api/v1/convertFile")
 public class RouterController {
 
-    @PostMapping()
-    public ResponseEntity<?> convertPDFFileEndpoint(@RequestParam("file") MultipartFile file) {
-        return ResponseEntity.ok("Test");
+    @PostMapping("/{extension}")
+    public ResponseEntity<?> convertPDFFileEndpoint(@PathVariable String extension,
+                                                    @RequestParam("file") MultipartFile file
+                                                    ) {
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        MultiValueMap<String, Object> multiValueMap =
+                new LinkedMultiValueMap<>();
+
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        multiValueMap.add("file", file.getResource());
+
+        HttpEntity<MultiValueMap<String, Object>> multiValueMapHttpEntity
+                = new HttpEntity<>(multiValueMap, headers);
+
+
+        ResponseEntity<byte[]> responseEntity =
+                restTemplate.postForEntity("http://localhost:8082/api/v1/fileToPDF"
+                        , multiValueMapHttpEntity
+                        , byte[].class);
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(responseEntity.getBody());
     }
 }
