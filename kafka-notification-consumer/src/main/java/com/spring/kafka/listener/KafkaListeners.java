aaa32c5c9661527fdff4985400c894ee54684c4d
albert-atabekyan <albert.atabekyan1200@gmail.com>
2022-08-10 17:05:34 +0300

    add kafka listener, consumer config, properties

---
+++ kafka-notification-consumer/src/main/java/com/spring/kafka/listener/KafkaListeners.java
@@ -0,0 +1,15 @@
+package com.spring.kafka.listener;
+
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaListeners {
+
+    @KafkaListener(topics = "test", groupId = "groupId")
+    void listener(String data) {
+        System.out.println("Message received " + data);
+
+    }
+}
